bbbc9576c7a6bf925a35b93151a1803d69f81267
LiamMcColeman <liammccoleman.info@gmail.com>
2024-12-24 22:38:31 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import Navbar from "./Navbar"
+function App() {
+
+  return (
+    <>
+    <div className="pb-8 flex">
+      <Navbar></Navbar>
+      </div>
+      <div>
+        
+        <h1>
+          Hello, World!
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Navbar.tsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <>
+        <div>
+            <ul>
+                <li>Home</li>
+                <li>Email</li>
+            </ul>
+        </div>
+        </>
+    )
+}
+
+export default Navbar
